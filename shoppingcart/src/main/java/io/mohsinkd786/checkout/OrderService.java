7c4171911bd392ceac04ade213210b6a6db8266e
sunil <indori.sunil@gmail.com>
2020-06-24 00:37:28 +0530

    merge changes

--- shoppingcart/src/main/java/io/mohsinkd786/checkout/OrderService.java
+++ shoppingcart/src/main/java/io/mohsinkd786/checkout/OrderService.java
@@ -1,4 +1,9 @@
 package io.mohsinkd786.checkout;
 
+import io.mohsinkd786.products.Product;
+
 public class OrderService {
+    public boolean addCartItems(Product p) {
+        return true;
+    }
 }
--- shoppingcart/src/test/java/io/mohsinkd786/checkout/OrderServiceTest.java
+++ shoppingcart/src/test/java/io/mohsinkd786/checkout/OrderServiceTest.java
@@ -1,5 +1,7 @@
 package io.mohsinkd786.checkout;
+import static org.junit.jupiter.api.Assertions.*;
 
+import io.mohsinkd786.products.Product;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
@@ -7,7 +9,8 @@ public class OrderServiceTest {
     @Test
     void testAddCartItems(){
         OrderService orderService = new OrderService();
-        //orderService.addCartItems();
+        Product p1=new Product(1,"Beer",2,12.30);
+        assertTrue(orderService.addCartItems(p1));
 
     }
 }
