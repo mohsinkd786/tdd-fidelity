06b066a42270b783b0d559e8a0c20a30ea4b20f1
techmomo <mohsin.khursheed@techjini.com>
2020-06-30 12:20:56 +0530

    Code Refactoring

--- product-service/src/main/java/io/mohsinkd786/services/impl/OrderServiceImpl.java
+++ product-service/src/main/java/io/mohsinkd786/services/impl/OrderServiceImpl.java
@@ -1,18 +1,27 @@
 package io.mohsinkd786.services.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderServiceImpl {
 
+    private ProcessOrderServiceImpl processOrderService;
+
+    @Autowired
+    public OrderServiceImpl(ProcessOrderServiceImpl processOrderService) {
+        this.processOrderService = processOrderService;
+    }
+
     public double processOrder(int quantity) {
-        double basePrice = 10;
-        double discount = 5;
-        double sellingPrice = 12;
-        boolean itemInStock = isItemInStock("SAMPLE");
+        //double finalPrice = processOrderService.calculateOrderCost(quantity);
+        //double totalPrice = finalPrice * quantity;
+        return calculateCost(quantity);
+    }
 
-        if (itemInStock) {
-            if (quantity > 3) {
-
-            }
-
+    private double calculateCost(int quantity) {
+        if (isItemInStock("SAMPLE")) {
+            return processOrderService.calculateOrderCost(quantity) * quantity;
         }
         return 0;
     }
